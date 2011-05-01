9cc1be60a52c15bfe4dcc04eaedf606b0e08a333
Kurios Creative Design <glenn@kurioscreative.com>
2011-05-01 17:24:42 -0400

    Working RSVP and member add

--- app/models/member_rsvp.rb
+++ app/models/member_rsvp.rb
@@ -3,14 +3,15 @@
 #
 # Table name: member_rsvps
 #
-#  id           :integer         not null, primary key
-#  member_id    :integer
-#  response     :string(255)
-#  meetup_id    :integer
-#  rsvp_updated :datetime
-#  event_id     :integer
-#  created_at   :datetime
-#  updated_at   :datetime
+#  id               :integer         not null, primary key
+#  member_id        :integer
+#  response         :string(255)
+#  meetup_id        :integer
+#  meetup_member_id :integer
+#  rsvp_updated     :datetime
+#  event_id         :integer
+#  created_at       :datetime
+#  updated_at       :datetime
 #
 
 class MemberRsvp < ActiveRecord::Base
@@ -19,15 +20,15 @@ class MemberRsvp < ActiveRecord::Base
   after_create :create_member
   
   def create_member
-    begin
+    if
       find_member
-    rescue
-      Member.create!(:meetup_id => meetup_id)
+    else
+      Member.create!(:meetup_id => meetup_member_id)
     end
   end
   
   def find_member
-    Member.find_by_meetup_id(meetup_member_id)
+    member = Member.find_by_meetup_id(meetup_member_id)
   end
   
 end
