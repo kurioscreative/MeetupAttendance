f83797c261e5be31188413e7fd01aa82754615d0
Kurios Creative Design <glenn@kurioscreative.com>
2011-05-17 20:30:27 -0400

    Events list only shows untracked events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,14 @@ class EventsController < ApplicationController
   
   def index
     @group = Group.find(params[:group_id])
-    fetched_events = RMeetup::Client.fetch(:events,    
+    fetched_events ||= RMeetup::Client.fetch(:events,    
         { :group_id => @group.meetup_id, 
           :after => "-1d"
         })
-    @events ||= Event.all    
-    @meetup_events = fetched_events.keep_if {|event| event.id != @events.collect(&:meetup_id)}
+    @events ||= @group.events.all   
+    tracked_ids = @events.collect { |x| x.meetup_id }
+
+    @meetup_events = fetched_events.select { |x| x unless tracked_ids.include?(x.id) }
   end
 
   def create
@@ -27,6 +29,10 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @group = Group.find(params[:group_id])
-    @rsvps = RMeetup::Client.fetch(:rsvps, {:event_id => @event.meetup_id})
+    @member_ids = MemberRsvp.all.collect{|x|x.meetup_id.to_i}
+    @rsvps = RMeetup::Client.fetch(:rsvps, {:event_id => @event.meetup_id}).keep_if { |x| x.id.to_i == @member_ids }
+    @members = MemberRsvp.scoped_by_event_id(@event.id).all
+    
+    # Get a list of all RMeetup:RSVPS from meetup and remove RMeetup:Rsvps whose id matches an existing MemberRsvp's id
   end
 end
